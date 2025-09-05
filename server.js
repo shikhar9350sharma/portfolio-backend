253a862f70caed6de2618f48246aebd4f66c1e53
shikhar9350sharma <shikharsharma593@gmail.com>
2025-09-05 12:04:57 +0530

    cors

--- server.js
+++ server.js
@@ -56,7 +56,11 @@ import cors from 'cors'
 const app = express();
 const port = 3000;
 dotenv.config()
-app.use(cors());
+app.use(cors({
+  origin: "https://portfolio-chi-red-69.vercel.app",
+  methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type"],
+}));
 
 app.use(express.json()); // Modern way to parse JSON
 
